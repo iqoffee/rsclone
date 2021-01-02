f3fe9e6e89fb2c109bfcd1f5daaec3ce75c1dd5e
iqoffee <iqoffee@users.noreply.github.com>
2021-01-02 13:50:30 +0500

    feat: getting data in request body

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-import express from express;
+//qqxSCdtQRc4xEaWt
+const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURL} = require('./keys')
+
+require('./models/user')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+mongoose.connect(MONGOURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () =>{
+	console.log('connected to mongoDB')
+})
+
+mongoose.connection.on('error', (err) =>{
+	console.log('error connecting to mongoDB', err)
+})
+
+app.listen(PORT, () => {
+    console.log('server in running', PORT)
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	MONGOURL: 'mongodb+srv://iqoffee:qqxSCdtQRc4xEaWt@cluster0.baar6.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
