54bb5e5aaa8b08fbc8b95ebb15a3d97eafca8282
iqoffee <iqoffee@users.noreply.github.com>
2021-01-04 12:37:49 +0500

    feat: Creating Sign in/Login route

--- routes/auth.js
+++ routes/auth.js
@@ -5,45 +5,73 @@ const User = mongoose.model('User')
 const bcrypt = require('bcryptjs')
 
 router.get('/',(req, res) => {
-	res.send('Hello')
+    res.send('Hello')
 })
 
 router.post('/signup', (req, res) => {
-	const {name, email, password} = req.body
+    const {name, email, password} = req.body
 
-	if(!email || !password || !name){
-		return res.status(422).json({error: 'please add all the fields'})
-	}
+    if(!email || !password || !name){
+        return res.status(422).json({error: 'please add all the fields'})
+    }
 
-	User.findOne({email: email})
-	.then((savedUser) => {
-		if (savedUser){
-			return res.status(422).json({error: 'user already exist whith that email'})
-		}
+    User.findOne({email: email})
+    .then((savedUser) => {
+        if (savedUser){
+            return res.status(422).json({error: 'user already exist whith that email'})
+        }
 
-		bcrypt.hash(password, 12)
-		.then(hashedpassword => {
+        bcrypt.hash(password, 12)
+        .then(hashedpassword => {
 
-			const user = new User({
-				email,
-				password: hashedpassword,
-				name
-			})	
+            const user = new User({
+                email,
+                password: hashedpassword,
+                name
+            })  
 
-			user.save()
-			.then(user => {
-				res.json({message: 'saved successfully'})
-			})
-			.catch(err => {
-				console.log(err)
-			})
+            user.save()
+            .then(user => {
+                res.json({message: 'saved successfully'})
+            })
+            .catch(err => { 
+                console.log(err)
+            })
 
-		})		
+        })      
 
-	})
-	.catch(err => {
-		console.log(err)
-	})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+router.post('/signin', (req, res) => {
+    const {email, password} = req.body
+
+    if (!email || !password){
+        res.status(422).json({error: 'please provide email or password'})
+    }
+
+    User.findOne({email: email})
+    .then(savedUser =>{
+        if(!savedUser){
+            return res.status(422).json({error: 'Invalid email or password'})
+        }
+
+        bcrypt.compare(password, savedUser.password)
+        .then(doMatch => {
+            if (doMatch) {
+                res.json({message: 'Successfully signed in'})
+            }
+            else {
+                return res.status(422).json({error: 'Invalid email or password'})
+            }
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    })
 })
 
 module.exports = router
