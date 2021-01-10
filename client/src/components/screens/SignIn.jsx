8a33b3b27da839d3f0a165a091f30509a7abe27c
iqoffee <iqoffee@users.noreply.github.com>
2021-01-10 07:47:38 +0500

    feat: user sign app logic and email vaalidation

--- client/src/components/screens/SignIn.jsx
+++ client/src/components/screens/SignIn.jsx
@@ -1,30 +1,22 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-    return (
-        <main className="mycard">
-            <div className="card auth-card input-field">
-                <h2>Instagram</h2>
-                <input 
-                type="text"
-                placeholder="email"
-                />
-                <input
-                 type="text"
-                placeholder="password"
-                />
-                  <button className="btn waves-effect waves-light blue lighten-2">
-                    Sign In                  
-                  </button>
-                  <h5>
-                      <Link to="/signup">
-                          Don't have an account?
-                      </Link>
-                  </h5>
-            </div>
-        </main>
-    )
-}
+  return (
+    <main className='mycard'>
+      <div className='card auth-card input-field'>
+        <h2>Instagram</h2>
+        <input type='text' placeholder='email' />
+        <input type='text' placeholder='password' />
+        <button className='btn waves-effect waves-light blue lighten-2'>
+          Sign In
+        </button>
+        <h5>
+          <Link to='/signup'>Don't have an account?</Link>
+        </h5>
+      </div>
+    </main>
+  );
+};
 
-export default SignIn
+export default SignIn;
